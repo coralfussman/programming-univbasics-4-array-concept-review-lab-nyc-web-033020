9246dab47e0efdb1626c02a8e62a6c07babbf82a
Coral Fussman <coralfussman@gmail.com>
2020-03-17 02:46:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,21 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  count = 0
+while count < array.length do
+  array.index(value_to_find)
+  count += 1
+end
 end
 
+
+
+#
+# def find_element_index(array, value_to_find)
+#   array.index(value_to_find)
+# end
+
 def find_max_value(array)
-  # Add your solution here
+array.max_by(length)
+
 end
 
 def find_min_value(array)
