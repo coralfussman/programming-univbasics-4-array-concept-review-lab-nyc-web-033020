6dc9895b2c89b2cb8e996dd832794e64451472cf
Coral Fussman <coralfussman@gmail.com>
2020-03-17 02:54:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
-  count = 0
-while count < array.length do
-  array.index(value_to_find)
-  count += 1
+
+count < array.length do
+  array.length.times do |index|
+if   index == value_to_find
+  return index
 end
 end
 
