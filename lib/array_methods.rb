bf76f14d08f4cf1ec5d6fe412fcd8e044614af30
Coral Fussman <coralfussman@gmail.com>
2020-03-17 03:18:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,8 @@
 def find_element_index(array, value_to_find)
 
   array.length.times do |index|
-    if array[count] == value_to_find
-     puts index
-   else
-     return nil
+    if array[index] == value_to_find
+     return index
    end
   end
 end
