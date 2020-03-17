5ac42a311f18422e3b12b08f08ddf1475ad3d6d1
Coral Fussman <coralfussman@gmail.com>
2020-03-17 03:51:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -27,12 +27,12 @@ x
 end
 
 def find_min_value(array)
-  x = 0
-array.length.times {|index|
+  x = array[0]
+  array.length.times {|index|
   if array[index] < x
     x = array[index]
   end
 
-}
-x
-end
+  }
+  x
+  end
